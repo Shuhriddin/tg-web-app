72f87de40c6e6884f3bb95a60e5179cadbc8cf49
shuxriddin <shuxriddinsolixov2023@gmail.com>
2023-11-29 21:19:22 +0500

    changeFile

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
 import { userTelegram } from "../../hooks/userTelegram";
 
@@ -9,7 +9,7 @@ function App() {
 
   useEffect(()=> {
     tg.ready()
-  })
+  }, [])
 
   
   return (
