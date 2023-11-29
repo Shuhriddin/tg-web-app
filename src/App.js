9450a6a24eb72fe7d9dee8b0302095d48a555f09
shuxriddin <shuxriddinsolixov2023@gmail.com>
2023-11-29 13:17:00 +0500

    intial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
 
+const tg = window.Telegram.WebApp;
+
 function App() {
+
+  useEffect(()=> {
+    tg.ready()
+  })
+
+  const onClose = () => {
+    tg.close()
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      work
+      <button onClick={onClose}>Закрить</button>
     </div>
   );
 }
