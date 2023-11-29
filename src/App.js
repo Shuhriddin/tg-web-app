a8b9821770bd3193c423f65fde245d88d235b644
shuxriddin <shuxriddinsolixov2023@gmail.com>
2023-11-29 14:36:44 +0500

    Revert "intial"
    This reverts commit 9450a6a24eb72fe7d9dee8b0302095d48a555f09.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import React, { useEffect } from 'react';
+import logo from './logo.svg';
 import './App.css';
 
-const tg = window.Telegram.WebApp;
-
 function App() {
-
-  useEffect(()=> {
-    tg.ready()
-  })
-
-  const onClose = () => {
-    tg.close()
-
-  }
   return (
     <div className="App">
-      work
-      <button onClick={onClose}>Закрить</button>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
